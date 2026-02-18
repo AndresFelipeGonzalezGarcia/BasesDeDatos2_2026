604a62620f4a79939ea6095b97048313af7c8fc9
AndresFelipeGonzalezGarcia <afegonzalezg@unbosque.edu.co>
2026-02-18 17:36:20 -0500

    nUEVOS SCRIPTS

--- PL/SQL/PL/SQL.sql
+++ PL/SQL/PL/SQL.sql
@@ -72,3 +72,90 @@ dbms_output.put_line('El nombre del empleado es: '
                      || vv_empleado.FIRST_NAME); --> Si quiero llamar un dato especifico de la base
         end;
 /
+
+-- Estructuras de control
+-- IF
+DECLARE
+    vd_current_date NUMBER := TO_NUMBER(TO_CHAR(SYSDATE, 'DD')); 
+BEGIN
+    IF vd_current_date IN (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31) THEN
+        dbms_output.put_line('HOLA PRIMO');
+    ELSE 
+        dbms_output.put_line('NO ES PRIMO');
+    END IF; 
+END;
+/
+-- GOTO
+DECLARE
+    vd_current_date NUMBER := TO_NUMBER(TO_CHAR(SYSDATE, 'DD')); 
+BEGIN
+    IF vd_current_date IN (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31) THEN
+        GOTO primo;
+    ELSE 
+        GOTO noprimo;
+    END IF; 
+
+-- LOOP
+ 
+DECLARE 
+    vn_a NUMBER := 0;
+    vn_b NUMBER := 1;
+    vn_c NUMBER;
+BEGIN
+ 
+    loop 
+    EXIT WHEN a > 100;
+    DBMS_OUTPUT.PUT_LINE(a);
+    vn_c := vn_a + vn_b;
+    vn_a := vn_b;
+    vn_b := vn_c;
+    END LOOP;
+END;
+/
+
+-- WHILE 
+DECLARE
+   a NUMBER := 12;
+   b NUMBER := 18;
+   mcd NUMBER;
+   mcm NUMBER;
+   x NUMBER;
+   y NUMBER;
+BEGIN
+   x := a;
+   y := b;
+ 
+ 
+   WHILE y != 0 LOOP
+      mcd := MOD(x, y);
+      x := y;
+      y := mcd;
+   END LOOP;
+ 
+   mcd := x;
+ 
+ 
+   mcm := (a * b) / mcd;
+ 
+   DBMS_OUTPUT.PUT_LINE('El MCM es: ' || mcm);
+END;
+/
+-- FOR
+BEGIN
+FOR X IN REVERSE 1..50 LOOP
+	dbms_output.put_line(2*X);
+	END LOOP;
+END;
+
+DECLARE
+    vn_numerouno NUMBER := 49;
+    vn_numeroraiz NUMBER := 0;
+ 
+BEGIN
+    FOR vn_numeroraiz IN 1..SQRT(vn_numerouno) LOOP
+        IF ((vn_numeroraiz * vn_numeroraiz)= vn_numerouno) THEN 
+            dbms_output.put_line(vn_numeroraiz);
+        END IF;
+    END LOOP;
+END;
+/
